5d3dc96848ed63cbfba6af1b886a658be831560f
Claudio Rivera <claudio@foundrymakes.com>
2022-08-02 10:34:11 -0500

    🚧 Proxy api requests

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,12 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path",
+        destination: `${process.env.API_URL}/:path`,
+      },
+    ];
+  },
 });
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,19 @@
+import useSWR from "swr";
 import { Button } from "ui";
 
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
 export default function Web() {
+  const { data, error } = useSWR("/api/todos", fetcher);
+  const isLoading = !error && !data;
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Oops...</p>;
+
   return (
     <div>
-      <h1>Web</h1>
       <Button />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
