338e6adbd016a53902bc0d7b880fea9d5a058a69
Claudio Rivera <claudio@foundrymakes.com>
2022-08-03 16:02:12 -0500

    🚧 Update web env vars

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,7 +6,9 @@ module.exports = withTM({
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_URL}/:path*`,
+        destination: `${
+          process.env.API_URL ?? process.env.API_RENDER_URL
+        }/:path*`,
       },
     ];
   },
